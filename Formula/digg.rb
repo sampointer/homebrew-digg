df674d21457bfdc4421152e1277188a13fb22cdf
goreleaserbot <sam@outsidethe.net>
2021-08-03 15:37:49 +0100

    Brew formula update for digg version v0.2.5

--- Formula/digg.rb
+++ Formula/digg.rb
@@ -5,24 +5,24 @@
 class Digg < Formula
   desc "Look up region and other information for any Google IP address"
   homepage "https://github.com/sampointer/digg"
-  version "0.2.4"
+  version "0.2.5"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sampointer/digg/releases/download/v0.2.4/digg_0.2.4_Darwin_x86_64.tar.gz"
-      sha256 "65b4b49f31e7f90130658b6787caa2b19aa09e60e36e454ade10dd19e8e2ac91"
+      url "https://github.com/sampointer/digg/releases/download/v0.2.5/digg_0.2.5_Darwin_x86_64.tar.gz"
+      sha256 "55edcc57cb7ca28236427a8f875f61dcbe8b5eb008ef551ec3b27fb1be50c0f8"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sampointer/digg/releases/download/v0.2.4/digg_0.2.4_Linux_x86_64.tar.gz"
-      sha256 "605e63dbdb274d01a7a372705b17afe1c17fce04f459a9cc94af7cd9469ea8eb"
+      url "https://github.com/sampointer/digg/releases/download/v0.2.5/digg_0.2.5_Linux_x86_64.tar.gz"
+      sha256 "8b2864583ca74920668e88e837c2775ac428ed3835dcef1c5956f77fa3fca391"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sampointer/digg/releases/download/v0.2.4/digg_0.2.4_Linux_arm64.tar.gz"
-      sha256 "08586e668ff8adde2390dbb7056d4d7c40fa3a1692e5c5d0725616c1ea4d5095"
+      url "https://github.com/sampointer/digg/releases/download/v0.2.5/digg_0.2.5_Linux_arm64.tar.gz"
+      sha256 "59689d8646765d88e6ce103f93ca7230c2e525aa8cf15a427becd26fdc4999d4"
     end
   end
 
